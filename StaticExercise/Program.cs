13828a06cd96e68be7fe5fa396010804a22dd90b
Filip Herbst <filip_herbst@yahoo.com>
2024-08-26 14:00:19 -0500

    Completed Exercise 1 - Temperature Conversion

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace StaticExercise
+﻿using System.Net;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Fahrenheit to Celsius - User Input, Method Call, and Output
+            Console.WriteLine("Enter a temperature in Fahrenheit to convert to Celsius.");
+            var parsedSuccess = double.TryParse(Console.ReadLine(), out var tempInput);
+
+            while (!parsedSuccess)
+            {
+                Console.WriteLine("Please enter a valid number:");
+                parsedSuccess = double.TryParse(Console.ReadLine(), out tempInput);
+            }
+            
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(tempInput)); 
+            
+            
+            //Celsius to Fahrenheit - User Input, Method Call, and Output
+            Console.WriteLine("Now enter a temperature in Celsius to convert to Fahrenheit.");
+            var parsedSuccess2 = double.TryParse(Console.ReadLine(), out var tempInput2);
+
+            while (!parsedSuccess2)
+            {
+                Console.WriteLine("Please enter a valid number:");
+                parsedSuccess2 = double.TryParse(Console.ReadLine(), out tempInput2);
+            }
+            
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(tempInput2));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace StaticExercise;
+
+public static class TempConverter
+{
+    public static double FahrenheitToCelsius(double inputTempF)
+    {
+        //double outputTempC1 = (inputTempF1 - 32) * 5 / 9;
+        //return outputTempC1;
+        return (inputTempF - 32) * 5 / 9;
+    }
+
+    public static double CelsiusToFahrenheit(double inputTempC)
+    {
+        //double outputTempF2 = (inputTempC2 * 9 / 5) + 32;
+        //return outputTempF2;
+        return (inputTempC * 9 / 5) + 32;
+    }
+}
